67e36813ab989527fa024e7ddcf8b6019831238a
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2023-10-12 16:23:34 +0700

    feat: add logic face recog

--- src/app/page.js
+++ src/app/page.js
@@ -21,27 +21,34 @@ export default function Home() {
 
     const faceMyDetect = () => {
         setInterval(async () => {
-            const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions());
+            // const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions());
+            const detections = await faceapi.detectSingleFace(videoRef.current, new faceapi.SsdMobilenetv1Options());
             // .withFaceLandmarks()
             // .withFaceExpressions();
 
-            if (detections.length !== 0) {
-                console.log("terdeteksi wajah");
+            // console.log("deteksi muka ", detections);
+            if (!detections) {
+                console.log("tidak terdeteksi wajah, tidak mengirim");
+            } else {
+                if (detections?.classScore) {
+                    console.log("terdeteksi wajah");
+                    // DRAW YOU FACE IN WEBCAM
+                    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current);
+                    faceapi.matchDimensions(canvasRef.current, {
+                        width: 650,
+                        height: 600,
+                    });
+
+                    const resized = faceapi.resizeResults(detections, {
+                        width: 650,
+                        height: 600,
+                    });
+
+                    faceapi.draw.drawDetections(canvasRef.current, resized);
+                    //Running Logic
+                }
             }
 
-            // DRAW YOU FACE IN WEBCAM
-            canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current);
-            faceapi.matchDimensions(canvasRef.current, {
-                width: 940,
-                height: 650,
-            });
-
-            const resized = faceapi.resizeResults(detections, {
-                width: 940,
-                height: 650,
-            });
-
-            faceapi.draw.drawDetections(canvasRef.current, resized);
             // faceapi.draw.drawFaceLandmarks(canvasRef.current, resized);
             // faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
         }, 1000);
@@ -52,9 +59,10 @@ export default function Home() {
         const loadModels = () => {
             Promise.all([
                 // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
-                faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+                // faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+                faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
                 // faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-                faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+                // faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
                 // faceapi.nets.faceExpressionNet.loadFromUri("/models"),
             ]).then(() => {
                 faceMyDetect();
@@ -72,7 +80,7 @@ export default function Home() {
             <div className="appvide">
                 <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
             </div>
-            <canvas ref={canvasRef} width="940" height="650" className="appcanvas" />
+            <canvas ref={canvasRef} width="650" height="600" className="appcanvas" />
         </div>
     );
 }
